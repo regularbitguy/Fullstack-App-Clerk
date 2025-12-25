65ad60edf91f0fbbe9d60c6021721b173a22f22d
Piero <piero.villanueva20@gmail.com>
2025-12-24 19:19:11 -0500

    Menú Sign-In , Sign-Up

---
+++ frontend/src/App.jsx
@@ -0,0 +1,24 @@
+import ClerkProviderWithRoutes from './auth/ClerkProviderWithRoutes.jsx'
+import { Routes, Route } from 'react-router-dom'
+import { Layout } from './layout/Layout.jsx'
+import { ChallengeGenerator } from './challenge/ChallengeGenerator.jsx'
+import { HistoryPanel } from './history/HistoryPanel.jsx'
+import { AuthenticationPage } from './auth/AuthenticationPage.jsx'
+import './App.css'
+
+function App() {
+  return <ClerkProviderWithRoutes>
+    <Routes>
+      <Route path ="/sign-in/*" element ={<AuthenticationPage/>}></Route>
+      <Route path ="/sign-up/" element ={<AuthenticationPage/>}></Route>
+      <Route element ={<Layout/>}>
+        <Route path ='/' element ={<ChallengeGenerator/>}></Route>
+        <Route path ='/history' element ={<HistoryPanel/>}></Route>
+      </Route>
+    </Routes>
+  </ClerkProviderWithRoutes>
+}
+
+
+
+export default App
